46b1edc22adcd7df3ab41cb7a37e95e49eb58552
visual <visual@LAB44-08>
2023-01-24 17:39:02 +0300

    Добавьте файлы проекта.

---
+++ kur/kur/pageAdd.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace kur
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class pageAdd : ContentPage
+    {
+        private product currentProduct {get; set;}
+        public pageAdd()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Add(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button_Change(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Del(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ kur/kur/product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kur
+{
+    internal class product
+    {
+        public string Name { get; set; }
+        public int Sum { get; set; }
+        public string Image { get; set; }
+
+        public product(string name, int sum, string image) 
+        {
+            Name = name;
+            Sum = sum;
+            Image = image;
+        }
+    }
+    
+}
